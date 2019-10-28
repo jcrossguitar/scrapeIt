1cb726bdf9605b42886a6dc94ab95b45cc6e9ef5
jon <jcrossguitar@gmail.com>
2019-10-28 14:50:32 -0700

    i changed the link to reddit and a page on politics. I grabbed the articles with the proper heading and also the proper url. I made a summary section as well that should also grab a summary of the article as well. I added references to it in app, index, and article js.

--- public/app.js
+++ public/app.js
@@ -3,13 +3,14 @@ $.getJSON("/articles", function(data) {
     // For each one
     for (var i = 0; i < data.length; i++) {
       // Display the apropos information on the page
-      $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+      $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>"+ data[i].summary + "</p>");
     }
   });
   
   
   // Whenever someone clicks a p tag
   $(document).on("click", "p", function() {
+
     // Empty the notes from the note section
     // $("#notes").empty();
     // // Save the id from the p tag
--- server.js
+++ server.js
@@ -34,12 +34,12 @@ mongoose.connect("mongodb://localhost/", { useNewUrlParser: true });
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with axios
-  axios.get("http://www.echojs.com/").then(function(response) {
+  axios.get("https://www.reddit.com/r/politics/").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
     // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+    $("article h3").each(function(i, element) {
       // Save an empty result object
       var result = {};
 
@@ -50,6 +50,9 @@ app.get("/scrape", function(req, res) {
       result.link = $(this)
         .children("a")
         .attr("href");
+      result.summary = $(this)
+        .children("a")
+        .attr("p");
 
       // Create a new Article using the `result` object built from scraping
       db.Article.create(result)
